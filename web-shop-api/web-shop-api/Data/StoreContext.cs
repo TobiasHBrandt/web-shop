441eb98457af3a047323b10bccd61d0937ead134
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2022-10-21 23:06:45 +0200

    setup
    model dbcontext and connectionstring

---
+++ web-shop-api/web-shop-api/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_shop_api.Entities;
+
+namespace web_shop_api.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product>? products { get; set; }
+    }
+}
