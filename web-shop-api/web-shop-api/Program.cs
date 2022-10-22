6594d2b3a48de2f44589e480fa11fb9f2cf0c203
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2022-10-22 23:29:38 +0200

    fetch data in database

--- web-shop-api/web-shop-api/Program.cs
+++ web-shop-api/web-shop-api/Program.cs
@@ -11,8 +11,15 @@ opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddTransient<StoreContext>();
+//builder.Services.AddScoped<DBInitializer>();
 var app = builder.Build();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
+
+//    DBInitializer.Initialize(services);
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -26,5 +33,21 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetService<ILogger<Program>>();
+try
+{
+    context.Database.Migrate();
+    DBInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+
+    logger.LogError(ex, "Problem migrating data");
+}
+
+await app.RunAsync();
 
 app.Run();
