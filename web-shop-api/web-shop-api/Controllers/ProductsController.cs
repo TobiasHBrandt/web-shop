f8f3e0e369c98ef406f9d401427cd218287b377b
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2022-10-24 13:56:41 +0200

    api
    setup for getProduct and getProduct Id

---
+++ web-shop-api/web-shop-api/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web_shop_api.Data;
+using web_shop_api.Entities;
+
+namespace web_shop_api.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = context.products.ToList();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")] // api/products/3
+        public ActionResult<Product> GetProduct(int id)
+        {
+            return context.products.Find(id);
+        }
+
+    }
+}
